be3c8a9099a90d57cf7c7a711ff801685508d1ee
_dream <will.ferens@gmail.com>
2018-03-16 13:18:34 -0600

    added database

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,11 @@
 // Update with your config settings.
-
+require('.dotenv').load()
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/memory2'
   },
-
 
   production: {
     client: 'postgresql',
