bd62b488871dc37087bbd9666cfd1c562c9579e8
_dream <will.ferens@gmail.com>
2018-03-16 13:27:08 -0600

    lol queries

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
 require('.dotenv').load()
+
 module.exports = {
 
   development: {
--- queries.js
+++ queries.js
@@ -2,13 +2,36 @@ const database = require("./database-connection");
 
 module.exports = {
     list(){
+        return database('coffees')
+            .select()
     },
     read(id){
+        return database('coffees')
+            .select()
+            .where('id', id)
+            .first()
+
+
     },
-    create(coffee){
+    create(resolution){
+        return database('coffees')
+            .insert(resolution)
+            .returning('*')
+            .then(record => record[0])
+
+
     },
-    update(id, coffee){
+    update(id, resolution){
+        return database('coffees')
+            .update(resolution)
+            .where('id', id)
+            .returning('*')
+            .then(record => record[0])
     },
     delete(id){
+        return database('coffees')
+            .delete()
+            .where('id', id)
     }
-};
+
+}
