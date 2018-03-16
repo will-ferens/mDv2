f50aa42108d84ce988fdb3ce8d48e54e3f96b07d
_dream <will.ferens@gmail.com>
2018-03-16 13:43:19 -0600

    fuck

--- seeds/info.js
+++ seeds/info.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('coffees').insert([
-        {id: 1, name: 'Balck and Tan', roaster: 'Ink', aroma: 3},
+        {id: 1, name: 'Black and Tan', roaster: 'Ink', aroma: 3},
         {id: 2, name: 'Holiday Roast', roaster: 'Starbucks', aroma: 9},
         {id: 3, name: 'House Quake', roaster: 'Denver Coffee', aroma: 6}
       ]).then(() => {
